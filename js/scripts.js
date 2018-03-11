7183c683338af83707326212910983610d998243
Magdalena Gresk <magdagresk@onet.pl>
2018-03-11 13:13:31 -0400

    Add files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var b = prompt('Enter triangle base');
+var h = prompt('Enter triangle height');
+var triangleArea = (b * h/2);
+alert('area of your triangle is equal ' + triangleArea);
+console.log('area of your triangle is equal ' + triangleArea);
